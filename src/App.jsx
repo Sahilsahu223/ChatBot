95e9e5e10ba3f1c2351004fc3bfb2a81d0507ff5
Sahilsahu223 <166105124+Sahilsahu223@users.noreply.github.com>
2025-04-28 03:51:43 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [question, setQuestion] = useState("")
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer]= useState("")
  async function genAnswer(){
     console.log("loading...")
     const response = await axios (
       {
-        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=your_api_key"
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
        , method: "post",
        data : {
         "contents": [{
@@ -19,14 +20,13 @@ function App() {
        }
       }
     )
-
-
-    console.log(response['data']['candidates'][0]['content']['parts'][0]['text'])
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text'])
   }
   return (
     <>
     <h1>CHATBOT AI</h1>
-    <textarea value={question} cols="30" rows="10"></textarea>
+    <textarea value={question} onChange={(e)=>setQuestion(e.target.value)} cols="30" rows="10"></textarea>
+    <br />
     <button onClick={genAnswer}>Generate</button>
     </>
   )
